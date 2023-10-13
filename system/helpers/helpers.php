8f36d671df67aba23585f5eb626c577d3a00ce7c
Armin Tavakoli <armacc20@gmail.com>
2023-10-14 01:29:43 +0330

    Update system helpers file
    Add flash function

--- system/helpers/helpers.php
+++ system/helpers/helpers.php
@@ -16,3 +16,19 @@ function old($name)
     }
 }
 
+function flash($name, $message = null)
+{
+    if(empty($message))
+    {
+        if (isset($_SESSION["temporary_flash"][$name])) {
+            $temporary = $_SESSION["temporary_flash"][$name];
+            unset($_SESSION["temporary_flash"][$name]);
+            return $temporary;
+        }
+        else{
+            return false;
+        }
+    }else{
+        $_SESSION["flash"][$name] = $message;
+    }
+}
