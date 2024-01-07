4590e4183e380459acbed8d4ece6b37a452d2f67
Armin Tavakoli <armacc20@gmail.com>
2024-01-06 14:55:37 -1200

    Update HasFileValidationRules file
    Add fileValidation function

--- system/Request/Traits/HasFileValidationRules.php
+++ system/Request/Traits/HasFileValidationRules.php
@@ -1,2 +1,26 @@
 <?php
 
+namespace System\Request\Traits;
+
+
+trait HasFileValidationRules
+{
+    protected function fileValidation($name, $ruleArray)
+    {
+        foreach ($ruleArray as $rule) {
+            if ($rule == "required") {
+                $this->fileRequired($name);
+            } elseif (strpos($rule, "mimes:") === 0) {
+                $rule = str_replace('mimes:', "", $rule);
+                $rule = explode(',', $rule);
+                $this->fileType($name, $rule);
+            } elseif (strpos($rule, "max:") === 0) {
+                $rule = str_replace('max:', "", $rule);
+                $this->maxFile($name, $rule);
+            } elseif (strpos($rule, "min:") === 0) {
+                $rule = str_replace('min:', "", $rule);
+                $this->minFile($name, $rule);
+            }
+        }
+    }
+
