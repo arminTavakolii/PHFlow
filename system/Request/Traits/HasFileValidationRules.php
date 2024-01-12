b056121d6163d5b81d3326bd16be63c4ea23888a
armin tavakoli <armacc20@gmail.com>
2024-01-12 03:35:20 +0330

    Update maxFile function

--- system/Request/Traits/HasFileValidationRules.php
+++ system/Request/Traits/HasFileValidationRules.php
@@ -43,7 +43,12 @@ trait HasFileValidationRules
 
     protected function maxFile($name, $size)
     {
-        
+        $size = $size * 1024;
+        if ($this->checkFirstError($name) && $this->checkFileExist($name)) {
+            if($this->files[$name]['size'] > $size){
+                $this->setError($name, "$name size must be lower than " . ($size / 1024) . " kb");
+            }
+        }
     }
     
 }
