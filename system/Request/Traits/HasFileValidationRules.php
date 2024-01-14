6b02f92670a9f00d4ea2c10b8efc8881c4792046
armin tavakoli <armacc20@gmail.com>
2024-01-14 04:03:19 +0330

    Update minFile function

--- system/Request/Traits/HasFileValidationRules.php
+++ system/Request/Traits/HasFileValidationRules.php
@@ -52,8 +52,12 @@ trait HasFileValidationRules
     }
     protected function minFile($name, $size)
     {
-        
-        
+        $size = $size * 1024;
+        if ($this->checkFirstError($name) && $this->checkFileExist($name)) {
+            if($this->files[$name]['size'] < $size){
+                $this->setError($name, "$name size must be upper than " . ($size / 1024) . " kb");
+            }
+        }
     }
     
 }
