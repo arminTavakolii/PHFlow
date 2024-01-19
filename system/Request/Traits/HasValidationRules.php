142ce84d724b2e8cdec001f31dc537f74cd2e321
armin tavakoli <armacc20@gmail.com>
2024-01-18 18:10:35 -0800

    Update maxStr function

--- system/Request/Traits/HasValidationRules.php
+++ system/Request/Traits/HasValidationRules.php
@@ -8,7 +8,11 @@ trait HasValidationRules
 {
     protected function maxStr($name, $count)
     {
-        
+        if($this->checkFieldExist($name)){
+            if (strlen($this->request[$name]) >= $count && $this->checkFirstError($name)){
+                $this->setError($name, "$name max length equal or lower than $count character");
+            }
+        }
     }
 }
 
