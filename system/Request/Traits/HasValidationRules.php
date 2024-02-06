74640fa506d9c040912679515792d78333be622b
armin tavakoli <armacc20@gmail.com>
2024-02-06 05:05:08 +0330

    Update HasValidationRules trait
    Add confirm function

--- system/Request/Traits/HasValidationRules.php
+++ system/Request/Traits/HasValidationRules.php
@@ -8,8 +8,8 @@ trait HasValidationRules
 {
     protected function maxStr($name, $count)
     {
-        if($this->checkFieldExist($name)){
-            if (strlen($this->request[$name]) >= $count && $this->checkFirstError($name)){
+        if ($this->checkFieldExist($name)) {
+            if (strlen($this->request[$name]) >= $count && $this->checkFirstError($name)) {
                 $this->setError($name, "$name max length equal or lower than $count character");
             }
         }
@@ -17,8 +17,8 @@ trait HasValidationRules
 
     protected function minStr($name, $count)
     {
-        if($this->checkFieldExist($name)){
-            if (strlen($this->request[$name]) <= $count && $this->checkFirstError($name)){
+        if ($this->checkFieldExist($name)) {
+            if (strlen($this->request[$name]) <= $count && $this->checkFirstError($name)) {
                 $this->setError($name, "$name min length equal or upper than $count character");
             }
         }
@@ -26,51 +26,49 @@ trait HasValidationRules
 
     protected function required($name)
     {
-        if((!isset($this->request[$name]) || $this->request[$name] === '') && $this->checkFirstError($name)){
-            $this->setError($name,"$name is required");
+        if ((!isset($this->request[$name]) || $this->request[$name] === '') && $this->checkFirstError($name)) {
+            $this->setError($name, "$name is required");
         }
     }
 
     protected function number($name)
     {
-        if($this->checkFieldExist($name)){
-            if(!is_numeric($this->request[$name]) && $this->checkFirstError($name))
-            {
-                $this->setError($name,"$name must be number format");
+        if ($this->checkFieldExist($name)) {
+            if (!is_numeric($this->request[$name]) && $this->checkFirstError($name)) {
+                $this->setError($name, "$name must be number format");
             }
         }
     }
 
     protected function date($name)
     {
-        if($this->checkFieldExist($name)){
-            if(!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/",$this->request[$name]) && $this->checkFirstError($name)){
-             $this->setError($name,"$name must be date format");
+        if ($this->checkFieldExist($name)) {
+            if (!preg_match("/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])$/", $this->request[$name]) && $this->checkFirstError($name)) {
+                $this->setError($name, "$name must be date format");
             }
-         }
+        }
     }
 
     protected function email($name)
     {
-        if($this->checkFieldExist($name)){
-            if(!filter_var($this->request[$name], FILTER_VALIDATE_EMAIL) && $this->checkFirstError($name))
-            {
-                $this->setError($name,"$name must be email format");
+        if ($this->checkFieldExist($name)) {
+            if (!filter_var($this->request[$name], FILTER_VALIDATE_EMAIL) && $this->checkFirstError($name)) {
+                $this->setError($name, "$name must be email format");
             }
         }
     }
 
     public function existsIn($name, $table, $field = "id")
     {
-        if($this->checkFieldExist($name)){
-            if($this->checkFirstError($name)){
+        if ($this->checkFieldExist($name)) {
+            if ($this->checkFirstError($name)) {
                 $value = $this->$name;
                 $sql = "SELECT COUNT(*) FROM $table WHERE $field = ?";
                 $statement = DBConnection::getDBConnectionInstance()->prepare($sql);
                 $statement->execute([$value]);
                 $result = $statement->fetchColumn();
-                if($result == 0 || $result === false){
-                    $this->setError($name,"$name not already exist");
+                if ($result == 0 || $result === false) {
+                    $this->setError($name, "$name not already exist");
                 }
             }
         }
@@ -78,21 +76,24 @@ trait HasValidationRules
 
     public function unique($name, $table, $field = "id")
     {
-        if($this->checkFieldExist($name)){
-            if($this->checkFirstError($name)){
+        if ($this->checkFieldExist($name)) {
+            if ($this->checkFirstError($name)) {
                 $value = $this->$name;
                 $sql = "SELECT COUNT(*) FROM $table WHERE $field = ?";
                 $statement = DBConnection::getDBConnectionInstance()->prepare($sql);
                 $statement->execute([$value]);
                 $result = $statement->fetchColumn();
-                if($result != 0){
-                    $this->setError($name,"$name must be unique");
+                if ($result != 0) {
+                    $this->setError($name, "$name must be unique");
                 }
             }
         }
     }
 
+    protected function confirm($name)
+    {
+        
+        
+    }
+    
 }
-
-
-
