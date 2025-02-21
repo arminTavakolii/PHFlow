2a1b6a6b109aba3fa9bff75677135bd2063b861c
armin tavakoli <armacc20@gmail.com>
2025-02-20 20:23:04 -0700

    Update HasCRUD trait
    Add createMethod function

---
+++ system/Database/Traits/HasCRUD.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace System\Database\Traits;
+
+use System\Database\DBConnection\DBConnection;
+
+trait HasCRUD{
+
+    protected function createMethod($values){
+        
+    }
+
+
+}
